46b328b9fe102ca3789a150548a33ad9b725f6c8
vyshnevska <vyshnevska.n@gmail.com>
2015-07-31 17:46:19 +0300

    Simple Github auth app

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'sinatra', '1.4.6'
+gem 'json', '1.7.4'
+gem 'rest-client'
---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'rest-client'
+require 'json'
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+get '/' do
+  erb :index, :locals => {:client_id => CLIENT_ID}
+end
+
+get '/callback' do
+  # Temporary GitHub code...
+  session_code = request.env['rack.request.query_hash']['code']
+
+  # POST it back to GitHub
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                          {:client_id => CLIENT_ID,
+                           :client_secret => CLIENT_SECRET,
+                           :code => session_code},
+                           :accept => :json)
+
+  # extract the token and granted scopes
+  access_token = JSON.parse(result)['access_token']
+
+  # check if we were granted user:email scope
+  scopes = JSON.parse(result)['scope'].split(',')
+  has_user_email_scope = scopes.include? 'user:email'
+
+  # fetch user information
+  auth_result = JSON.parse(RestClient.get('https://api.github.com/user',
+                                          {:params => {:access_token => access_token}}))
+
+  # if the user authorized it, fetch private emails
+  if has_user_email_scope
+    auth_result['private_emails'] =
+      JSON.parse(RestClient.get('https://api.github.com/user/emails',
+                                {:params => {:access_token => access_token}}))
+  end
+
+  erb :basic, :locals => auth_result
+end
